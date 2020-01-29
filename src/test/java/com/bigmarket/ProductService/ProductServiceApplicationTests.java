cfe3c45a67ea8eab2327d477a33e6f486c1a8a8e
avinash-chavan <chavan.avinash0315@gmail.com>
2020-01-29 11:36:05 +0530

    initial commit

---
+++ src/test/java/com/bigmarket/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigmarket.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
